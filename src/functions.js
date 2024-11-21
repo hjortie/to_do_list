5ff6aab440380c2c72cb7e810c9a28290f7b2fd0
hjortie <elinhjortskull@live.se>
2024-11-21 14:26:35 +0100

    nu JÄVLAR

--- src/functions.js
+++ src/functions.js
@@ -2,6 +2,7 @@ import { Task } from "./models/Tasks";
 
 export function createTaskListHTML(taskList, listContainer, finishedTasks) {
     listContainer.innerHTML = "";
+    finishedTasks.innerHTML = "";
     for (let i = 0; i < taskList.length; i++) {
         const checker = document.createElement("input")
         const listItemName = document.createElement("li");
@@ -9,6 +10,7 @@ export function createTaskListHTML(taskList, listContainer, finishedTasks) {
         const listItemDeadL = document.createElement("p");
 
         checker.type = "checkbox";
+        checker.checked = taskList[i].isFinished;
         listItemName.innerHTML = taskList[i].taskName;
         listItemName.className = "task-name";
         listItemDescr.innerHTML = taskList[i].description;
@@ -21,21 +23,54 @@ export function createTaskListHTML(taskList, listContainer, finishedTasks) {
         listItemName.appendChild(listItemDescr);
         listItemName.appendChild(listItemDeadL);
 
+        if (taskList[i].isFinished === true) {
+            finishedTasks.appendChild(listItemName);
+            listItemName.className = "finished-tasks";
+
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerHTML = "Ta bort";
+            deleteBtn.className = "delete-btn";
+            finishedTasks.appendChild(deleteBtn);
+
+            deleteBtn.addEventListener("click", () => {
+                taskList.splice(i, 1);
+                localStorage.setItem("user_tasks", JSON.stringify(taskList));
+                createTaskListHTML(taskList, listContainer, finishedTasks)
+            });
+        }
+
+        else {
+            listItemName.className = "task-name";
+            listContainer.appendChild(listItemName);
+        }
 
         checker.addEventListener("change", () => {
-            taskList[i].isFinished = checker.checked;
-            const finishedTask = listItemName.parentNode.removeChild(listItemName);
-            finishedTask.className = "finished-tasks";
-
-            if (taskList[i].isFinished === true) {
-                finishedTasks.appendChild(listItemName);
+            if (checker.checked) {
+                taskList[i].isFinished = true;
+                //const finishedTask = listItemName.parentNode.removeChild(listItemName);
+                //finishedTask.className = "finished-tasks";
+                localStorage.setItem("user_tasks", JSON.stringify(taskList));
+                //finishedTasks.appendChild(listItemName);
+                createTaskListHTML(taskList, listContainer, finishedTasks);
             }
             else {
+                taskList[i].isFinished = false;
                 listItemName.className = "task-name";
                 listContainer.appendChild(listItemName);
+                localStorage.setItem("user_tasks", JSON.stringify(taskList));
             }
         })
+
+
     }
+    const sortBtn = document.createElement("button");
+    sortBtn.innerHTML = "Sortera";
+    listContainer.appendChild(sortBtn);
+
+    sortBtn.addEventListener("click", () => {
+        taskList.sort((a, b) => a.taskName.localeCompare(b.taskName));
+        createTaskListHTML(taskList, listContainer, finishedTasks);
+    })
 }
 
 export function saveNewTask(nameContainer, descContainer, dlContainer, taskList, listContainer, finishedTasks) {
@@ -45,10 +80,17 @@ export function saveNewTask(nameContainer, descContainer, dlContainer, taskList,
 
     let userTask = new Task(newItemName, newItemDesc, newItemDL, false);
     taskList.push(userTask);
+    localStorage.setItem("user_tasks", JSON.stringify(taskList));
     createTaskListHTML(taskList, listContainer, finishedTasks);
 }
 
-export function sortFunction(taskList, listContainer, finishedTasks) {
+export function sortTasks(taskList, listContainer, finishedTasks) {
     taskList.sort((a, b) => a.taskName.localeCompare(b.taskName));
     createTaskListHTML(taskList, listContainer, finishedTasks);
 }
+
+// function deleteTask(taskList, listContainer, finishedTasks) {
+//     taskList.splice(i, 1);
+//     localStorage.setItem("user_tasks", JSON.stringify(taskList));
+//     createTaskListHTML(taskList, listContainer, finishedTasks)
+// }
--- src/main.js
+++ src/main.js
@@ -15,7 +15,17 @@ const item2 = new Task("Diska", "Diskberget måste bort", "19:00 varje kväll", 
 const item3 = new Task("Tänka igenom saker", "Reflektera över tankar och känslor som slagit till under dagen", "21:00 varje kväll", false);
 const item4 = new Task("Plugga", "Hitta på ett eget projekt som använder Javascript och koda på!", "tisdag om två veckor", false);
 
-let toDoArray = [item1, item2, item3, item4];
+let toDoArray = [];
+
+const foundUserTasks = localStorage.getItem("user_tasks");
+const foundUserTasksObjectified = JSON.parse(foundUserTasks);
+
+if (foundUserTasksObjectified) {
+    toDoArray = foundUserTasksObjectified;
+}
+else {
+    toDoArray = [item1, item2, item3, item4];
+}
 
 createTaskListHTML(toDoArray, theList, allDoneList);
 
@@ -25,7 +35,6 @@ const dlContainer = document.getElementById("user-input-deadline");
 
 const saveBtn = document.getElementById("save-btn");
 saveBtn.addEventListener("click", () => { saveNewTask(nameContainer, descContainer, dlContainer, toDoArray, theList, allDoneList) });
-
 
 
 
